9f43ddac4e771e9c64ee728e3779cc7bd001f613
ecormaksin <truth_jp_4133@yahoo.co.jp>
2021-07-09 00:42:26 +0900

    fix create directory process

--- create_symlink.bat
+++ create_symlink.bat
@@ -1,6 +1,7 @@
 @echo off
 
 mkdir C:\ws\sl
+mkdir C:\ws\sl\.vscode
 mklink /d C:\ws\sl\.vscode\extensions %USERPROFILE%\.vscode\extensions
 mklink /d C:\ws\sl\.ssh %USERPROFILE%\.ssh
 mklink /d C:\ws\sl\jdk8 %USERPROFILE%\scoop\apps\openjdk8-redhat\current
