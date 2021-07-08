159570c0f0183d8c8f706c0430882a1f205fe999
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2021-07-08 10:18:46 +0900

    first commit

---
+++ create_symlink.bat
@@ -0,0 +1,8 @@
+@echo off
+
+mkdir C:\ws\sl
+mklink /d C:\ws\sl\.vscode\extensions %USERPROFILE%\.vscode\extensions
+mklink /d C:\ws\sl\.ssh %USERPROFILE%\.ssh
+mklink /d C:\ws\sl\jdk8 %USERPROFILE%\scoop\apps\openjdk8-redhat\current
+mklink /d C:\ws\sl\jdk11 %USERPROFILE%\scoop\apps\openjdk11\current
+mklink /d C:\ws\sl\gradle %USERPROFILE%\scoop\apps\gradle\current
